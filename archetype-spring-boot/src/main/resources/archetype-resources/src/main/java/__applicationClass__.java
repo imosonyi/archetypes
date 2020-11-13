1f27a8b2137d6e7eb6735a352d5a4562f2fd97c0
Istvan Mosonyi <istvan.mosonyi@bvtech.io>
2020-11-13 10:36:39 +0100

    Moved spring-boot to archetype-spring-boot

---
+++ archetype-spring-boot/src/main/resources/archetype-resources/src/main/java/__applicationClass__.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ${applicationClass} {
+
+    public static void main(String[] args) {
+        SpringApplication.run(${applicationClass}.class, args);
+    }
+
+}
