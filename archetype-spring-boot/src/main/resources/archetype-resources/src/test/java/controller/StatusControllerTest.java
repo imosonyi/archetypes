f54984d6b0c85e7b92cacefa99483208529f44f3
Istvan Mosonyi <istvan.mosonyi@bvtech.io>
2020-11-13 11:22:38 +0100

    Added jacoco test coverage

---
+++ archetype-spring-boot/src/main/resources/archetype-resources/src/test/java/controller/StatusControllerTest.java
@@ -0,0 +1,28 @@
+package ${package}.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(StatusController.class)
+public class StatusControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getStatusShouldReturnOK() throws Exception {
+        mockMvc.perform(get("/status"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("OK"));
+    }
+
+}
